7fe6da311b98252880181235b0142af73f2bcdcd
Méryl <meryl.valier@gmail.com>
2019-02-05 16:06:44 +0100

    init feature check password

---
+++ ValidorForm/ValidorForm/ValidorForm.swift
@@ -0,0 +1,14 @@
+//
+//  ValidorForm.swift
+//  ValidorForm
+//
+//  Created by Méryl VALIER on 05/02/2019.
+//  Copyright © 2019 Dinoboros. All rights reserved.
+//
+
+import Foundation
+
+protocol ValidorForm {
+    
+    static func checkPassword() -> Self
+}
